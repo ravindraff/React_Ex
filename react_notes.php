6a8833dece1274fad5787115dd1f3708ceb1fdd1
ravindraff <ravindra.m568@gmail.com>
2021-07-20 18:05:23 +0530

    sample

---
+++ react_notes.php
@@ -0,0 +1,41 @@
+download and install npm
+===========================
+npm is node pakaging manager
+
+after that install npm
+
+install yarn tool
+=====================
+npm install -g yarn@latest
+
+
+
+create-react-app tool using create react projects
+npm install -g create-react-app
+
+=============================================
+
+create folder
+switch to created folder
+>cd folder name
+>create-react-app first-app
+switch to folder
+>cd first-app
+start the project 
+>npm start/yarn start
+
+Git Commit:
+===================
+echo "# ReactPractices" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ravindraff/ReactPractices.git
+git push -u origin main
+
+yarn add @material-ui/core @material-ui/icons --save
+
+
+ 
+

e573faf00fd8a6888b0600f47b443b13ec6b3510
ravindraff <ravindra.m568@gmail.com>
2021-07-21 16:52:25 +0530

    component-ex

--- class_apps/comp-ex/src/Second.js
+++ class_apps/comp-ex/src/Second.js
@@ -1,5 +1,8 @@
 import React, { Fragment } from 'react';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HomeIcon from '@material-ui/icons/Home';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 function Second(){
@@ -19,7 +22,17 @@ function Second(){
             <Button color="primary" variant="contained">primary</Button>
             <Button color="secondary" variant="contained">secondary</Button>
             <Button color="danger" variant="contained">danger</Button>
-            <Button disabled>disabled</Button>
+            <Button disabled variant="contained">disabled</Button>
+            <br /><br />
+
+            <Button variant="outlined">Fisrt</Button>
+            <Button color="primary" variant="outlined">primary</Button>
+            <Button color="secondary" variant="outlined">secondary</Button>
+            <Button color="danger" variant="outlined">danger</Button>
+            <Button disabled variant="outlined">disabled</Button>
+            <Button variant="outlined" color="primary" href="#outlined-buttons">
+            Link
+            </Button>
             
             <br /><br />
             <ButtonGroup>
@@ -27,6 +40,29 @@ function Second(){
                 <Button color="secondary">secondary</Button>
                 <Button color="danger">danger</Button>
             </ButtonGroup>
+            <br></br>
+            <Button>Fisrt</Button>
+            <Button color="primary" size="small" variant="outlined">primary</Button>
+            <Button color="secondary" size="medium" variant="contained">secondary</Button>
+            <Button color="danger" size="large" variant="">danger</Button>
+            <br /><br />
+            <IconButton color="primary" size="large" variant="outlined">
+           {/*  import AccountCircleIcon from '@material-ui/icons/AccountCircle'; */}
+            <AccountCircleIcon />
+            </IconButton>
+            <br></br><br></br>
+            <Button variant="contained"
+                    color="primary"
+                    size="medium"
+                    startIcon={<AccountCircleIcon />}>
+                Account
+            </Button>
+            <Button variant="contained"
+                    color="secondary"
+                    size="medium"
+                    endIcon={<HomeIcon />}>
+                Home
+            </Button>
         </Fragment>
 
     )

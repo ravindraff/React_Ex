f674a06d492addd2bdef54bcbebb1f1bd8a87a2d
ravindraff <ravindra.m568@gmail.com>
2021-07-24 17:54:59 +0530

    create components

--- class_apps/crud-app-thunk/src/components/Header.tsx
+++ class_apps/crud-app-thunk/src/components/Header.tsx
@@ -1,14 +1,23 @@
-import { AppBar, Badge, Grid, IconButton, InputBase, Toolbar } from "@material-ui/core";
+import { AppBar, Badge, Grid, IconButton, InputBase, Toolbar ,makeStyles} from "@material-ui/core";
 import {  MailOutlined, NotificationsOff } from "@material-ui/icons";
 import SettingsPowerIcon from '@material-ui/icons/SettingsPower';
+import SearchIcon from '@material-ui/icons/Search';
 import React from "react";
+const useStyles = makeStyles({
+    root:{ 
+        background:"white"
+    }
+})
 function Header() {
+    const classes = useStyles();
     return(
-        <AppBar position="static">
+        <AppBar position="static" className={classes.root}>
             <Toolbar>
                 <Grid container>
                     <Grid item>
-                        <InputBase />
+                        <InputBase placeholder="Search Here" 
+                        startAdornment={<SearchIcon />}
+                        />
                     </Grid>
                     <Grid item sm={true}>
                     </Grid>
